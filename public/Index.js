998113c16addd90e2cdb19da0d60bf1e49c6ab9f
Zulkifal05 <zulkifalali299@gmail.com>
2025-07-26 19:38:53 +0500

    Public folder projects information added

---
+++ public/Index.js
@@ -0,0 +1,37 @@
+export const Projects = [
+    {
+        title : "Book Order Store",
+        Description : "A console based book ordering application built with C++. This was my BSCS first semester Programming Fundamental's final project.",
+        GithubLink : "https://github.com/Zulkifal05/Book-order-store.git"
+    },
+    {
+        title : "Railway Booking System",
+        Description : "A console based C++ application built using OOP concepts. This application uses File Handling for storing , updating & getting data.",
+        GithubLink : "https://github.com/Zulkifal05/Railway-Booking-System.git"
+    },
+    {
+        title : "Tic Tac Toe Game",
+        Description : "This game is built in HTML, CSS & JAVASCRIPT. It is my first project including Javascript But not the last. This strenghtened my JAVASCRIPT foundations.",
+        GithubLink : "https://github.com/Zulkifal05/Tic-Tac-Toe-Game.git"
+    },
+    {
+        title : "Rock Paper Scissors",
+        Description : "This game is built in HTML, CSS & JAVASCRIPT. I have used core JAVASCRIPT concepts in building this game.",
+        GithubLink : "https://github.com/Zulkifal05/Rock-Paper-Scissors-Game.git"
+    },
+    {
+        title : "Weather App",
+        Description : "The Weather App built in HTML , CSS & JAVASCRIPT. This App uses weather API for fetching live weather data.",
+        GithubLink : "https://github.com/Zulkifal05/Weather-Web-App.git"
+    },
+    {
+        title : "Bookit",
+        Description : "It is built in HTML , CSS & JAVASCRIPT. It is Booking site and a SPA with all the content dynamically rendered.",
+        GithubLink : "https://github.com/Zulkifal05/Bookit.git"
+    },
+    {
+        title : "Blogify",
+        Description : "Blogify is a Web App for Writing Blogs whose frontend is built in React JS & Appwrite is used as a backend service.",
+        GithubLink : "https://github.com/Zulkifal05/BlogApp.git"
+    }
+]
