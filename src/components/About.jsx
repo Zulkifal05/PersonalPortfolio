797d19ce9dfe8722bdcde50b81b46228b4e60f79
Zulkifal05 <zulkifalali299@gmail.com>
2025-07-30 22:28:23 +0500

    Development Completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import PROFILEPIC from "../assets/PROFILEPIC.png"
+import CppPic from "../assets/CppPic.png"
+import CSSPic from "../assets/CSSPic.png"
+import JavascriptPic from "../assets/JavascriptPic.png"
+import ReactPic from "../assets/ReactPIC.png"
+import TailwindPIC from "../assets/TailwindPIC.png"
+import HTMLPic from "../assets/HTMLPic.png"
+
 
 const About = ({AboutReference}) => {
   return (
@@ -6,7 +14,7 @@ const About = ({AboutReference}) => {
     ref={AboutReference}
     className='flex items-center justify-center gap-13 [@media(max-width:768px)]:flex-col mt-10 w-full px-30'>
       <div>
-        <img src="/public/Pics/PROFILE PIC.png" alt="Profile Pic" className='rounded-[13%] h-[50vh] w-300 min-w-[250px] [@media(max-width:360px)]:h-[30vh]'/>
+        <img src={PROFILEPIC} alt="Profile Pic" className='rounded-[13%] h-[50vh] w-300 min-w-[250px] [@media(max-width:360px)]:h-[30vh]'/>
       </div>
       <div className='min-w-[300px]'>
         <div>
@@ -15,12 +23,12 @@ const About = ({AboutReference}) => {
         </div>
 
         <div className='flex items-center justify-start gap-5 mt-5 flex-wrap'>
-          <img src="/public/Pics/HTML Pic.png" alt="HTML" className='h-10 rounded-lg'/>
-          <img src="/public/Pics/CSS Pic.png" alt="CSS" className='h-10 rounded-lg'/>
-          <img src="/public/Pics/Tailwind Pic.png" alt="Tailwind" className='h-10 rounded-lg'/>
-          <img src="/public/Pics/Javascript Pic.png" alt="JAVASCRIPT" className='h-10 rounded-lg'/>
-          <img src="/public/Pics/React Pic.png" alt="REACT" className='h-10 rounded-lg'/>
-          <img src="/public/Pics/C++ Pic.png" alt="C++" className='h-10 rounded-lg'/>
+          <img src={HTMLPic} alt="HTML" className='h-10 rounded-lg'/>
+          <img src={CSSPic} alt="CSS" className='h-10 rounded-lg'/>
+          <img src={TailwindPIC} alt="Tailwind" className='h-10 rounded-lg'/>
+          <img src={JavascriptPic} alt="JAVASCRIPT" className='h-10 rounded-lg'/>
+          <img src={ReactPic} alt="REACT" className='h-10 rounded-lg'/>
+          <img src={CppPic} alt="C++" className='h-10 rounded-lg'/>
         </div>
       </div>
     </div>
