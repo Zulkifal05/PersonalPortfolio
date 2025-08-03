36335f5265f63aff90d7a2c90e741cd6117c0afd
Zulkifal05 <zulkifalali299@gmail.com>
2025-08-03 20:02:18 +0500

    Skills section UI updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,5 @@
 import React from 'react'
 import PROFILEPIC from "../assets/PROFILEPIC.png"
-import CppPic from "../assets/CppPic.png"
-import CSSPic from "../assets/CSSPic.png"
-import JavascriptPic from "../assets/JavascriptPic.png"
-import ReactPic from "../assets/ReactPic.png"
-import TailwindPIC from "../assets/TailwindPic.png"
-import HTMLPic from "../assets/HTMLPic.png"
-
 
 const About = ({AboutReference}) => {
   return (
@@ -20,15 +13,6 @@ const About = ({AboutReference}) => {
         <div>
           <p className='font-bold text-white text-2xl [@media(max-width:768px)]:text-md'>Hi, I'm <span className='text-green-500 text-3xl font-bold'>Syed Zulkifal Ali Shah Bokhary </span>
           currently pursuing a Bachelor's degree in <span   className='text-green-500 text-3xl font-bold [@media(max-width:768px)]:text-md'>Computer Science</span> from <span className='text-green-500 text-3xl font-bold'>UET Taxila</span>. Passionate about technology, development, and problem-solving, I enjoy building creative and efficient solutions through code.</p>
-        </div>
-
-        <div className='flex items-center justify-start gap-5 mt-5 flex-wrap'>
-          <img src={HTMLPic} alt="HTML" className='h-10 rounded-lg'/>
-          <img src={CSSPic} alt="CSS" className='h-10 rounded-lg'/>
-          <img src={TailwindPIC} alt="Tailwind" className='h-10 rounded-lg'/>
-          <img src={JavascriptPic} alt="JAVASCRIPT" className='h-10 rounded-lg'/>
-          <img src={ReactPic} alt="REACT" className='h-10 rounded-lg'/>
-          <img src={CppPic} alt="C++" className='h-10 rounded-lg'/>
         </div>
       </div>
     </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,21 +1,43 @@
 import React from 'react'
+import CppPic from "../assets/CppPic.png"
+import CSSPic from "../assets/CSSPic.png"
+import JavascriptPic from "../assets/JavascriptPic.png"
+import ReactPic from "../assets/ReactPic.png"
+import TailwindPIC from "../assets/TailwindPic.png"
+import HTMLPic from "../assets/HTMLPic.png"
 
 const Skills = ({SkillsReference}) => {
   return (
     <div 
     ref={SkillsReference}
-    className='w-full h-[50vh] mt-13 flex items-center justify-center'>
-      <div className='w-[30%] h-[97%] rounded-2xl p-5 min-w-[300px] border-2 border-green-500'>
-        <h1 className='text-center font-bold text-green-500 text-3xl'>Skills</h1>
-        <div className='flex flex-col justify-evenly mt-5'>
-          <p className='font-bold text-white text-2xl mt-1'>HTML</p>
-          <p className='font-bold text-white text-2xl mt-1'>CSS</p>
-          <p className='font-bold text-white text-2xl mt-1'>Tailwind</p>
-          <p className='font-bold text-white text-2xl mt-1'>Javascript</p>
-          <p className='font-bold text-white text-2xl mt-1'>React JS</p>
-          <p className='font-bold text-white text-2xl mt-1'>C++</p>
+    className='w-full h-[63vh] mt-13 flex items-center justify-center flex-col'>
+    <h1 className='text-center font-bold text-green-500 text-4xl'>Skills</h1>
+    <div className='flex items-center justify-center flex-wrap mt-9 gap-9'>
+          <div className='flex flex-col items-center justify-center gap-3 outline-3 outline-green-500 min-w-[50px] w-30 p-3 rounded-lg'>
+            <img src={HTMLPic} alt="HTML Pic" className='h-13 rounded-lg'/>
+            <p className='font-bold text-center text-white'>HTML</p>
+          </div>
+          <div className='flex flex-col items-center justify-center gap-3 outline-3 outline-green-500 min-w-[50px] w-30 p-3 rounded-lg'>
+            <img src={CSSPic} alt="CSS Pic" className='h-13 rounded-lg'/>
+            <p className='font-bold text-center text-white'>CSS</p>
+          </div>
+          <div className='flex flex-col items-center justify-center gap-3 outline-3 outline-green-500 min-w-[50px] w-30 p-3 rounded-lg'>
+            <img src={TailwindPIC} alt="Tailwind Pic" className='h-13 rounded-lg'/>
+            <p className='font-bold text-center text-white'>Tailwind</p>
+          </div>
+          <div className='flex flex-col items-center justify-center gap-3 outline-3 outline-green-500 min-w-[50px] w-30 p-3 rounded-lg'>
+            <img src={JavascriptPic} alt="Javascript Pic" className='h-13 rounded-lg'/>
+            <p className='font-bold text-center text-white'>Javascript</p>
+          </div>
+          <div className='flex flex-col items-center justify-center gap-3 outline-3 outline-green-500 min-w-[50px] w-30 p-3 rounded-lg'>
+            <img src={ReactPic} alt="React Pic" className='h-13 rounded-lg'/>
+            <p className='font-bold text-center text-white'>React</p>
+          </div>
+          <div className='flex flex-col items-center justify-center gap-3 outline-3 outline-green-500 min-w-[50px] w-30 p-3 rounded-lg'>
+            <img src={CppPic} alt="C++ Pic" className='h-13 rounded-lg'/>
+            <p className='font-bold text-center text-white'>C++</p>
+          </div>
         </div>
-      </div>
     </div> 
   )
 }
