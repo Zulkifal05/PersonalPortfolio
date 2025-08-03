b638ad47d6b6e57bb5fb7c63d0999df5e50535d3
Zulkifal05 <zulkifalali299@gmail.com>
2025-08-03 20:10:17 +0500

    Main name change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,7 +26,7 @@ const Header = ({
   return (
     <div className='flex items-center py-3 px-10 h-20'>
       <div className='grow-1'>
-        <h1 className='text-green-500 font-bold text-3xl cursor-pointer'>Syed Zulkifal Ali</h1>
+        <h1 className='text-green-500 font-bold text-3xl cursor-pointer'>Syed Zulkifal</h1>
       </div>
       <div className='grow-2 flex items-center justify-evenly [@media(max-width:768px)]:hidden'>
         <button className='text-white hover:text-green-700 font-bold cursor-pointer text-xl' onClick={ScrollToAbout}>About Me</button>
