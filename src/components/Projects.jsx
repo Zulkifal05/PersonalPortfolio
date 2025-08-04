d40f2274edc361244b8c2ea5960a0fb1fc500734
Zulkifal05 <zulkifalali299@gmail.com>
2025-08-04 14:35:04 +0500

    Filter buttons at projects section UI updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,36 +3,45 @@ import { ProjectsArray } from '../../public/Projects'
 import ProjectCard from './ProjectCard'
 
 const Projects = ({ProjectsReference}) => {
-  let [selectValue,setSelectValue] = useState("All");
+  let [btnsValue,setBtnsValue] = useState("All");
   let [projects,setProjects] = useState(ProjectsArray);
   let [projectsToShow,setProjectsToShow] = useState(ProjectsArray);
 
   useEffect(() => {
-    if(selectValue === "All") {
+    if(btnsValue === "All") {
       setProjectsToShow(ProjectsArray);
     }
     else {
-      setProjectsToShow(projects.filter((project) => project.Type === selectValue))
+      setProjectsToShow(projects.filter((project) => project.Type === btnsValue))
     }
-  },[selectValue])
+  },[btnsValue])
 
-  function OnSelectChange(e) {
-    setSelectValue(e.target.value);
+  function AllBtnClick() {
+    setBtnsValue("All");
+  }
+
+  function JavascriptBtnClick() {
+    setBtnsValue("Javascript");
+  }
+
+  function ReacttBtnClick() {
+    setBtnsValue("React");
+  }
+
+  function CppBtnClick() {
+    setBtnsValue("C++");
   }
 
   return (
     <div ref={ProjectsReference}  className='mt-13 pt-20'>
       <h1 className='text-green-500 font-bold text-center text-4xl'>Projects</h1>
       <div className='flex items-center justify-center mt-5'>
-        <select 
-        onChange={(e) => OnSelectChange(e)}
-        value={selectValue}
-        className='border-none outline-2 outline-green-700 text-green-700 px-5 py-1 text-2xl rounded-md font-bold'>
-          <option value="All">All</option>
-          <option value="Javascript">Javascript</option>
-          <option value="React">React</option>
-          <option value="C++">C++</option>
-        </select>
+        <div className='flex items-center justify-center gap-5 flex-wrap'>
+          <button className={`text-green-500 border-none outline-2 outline-green-500 py-1 px-5 rounded-lg font-bold text-lg cursor-pointer ${ btnsValue === "All" ? "bg-green-500 text-white" : "" }`} onClick={AllBtnClick}>All</button>
+          <button className={`text-green-500 border-none outline-2 outline-green-500 py-1 px-5 rounded-lg font-bold text-lg cursor-pointer ${ btnsValue === "Javascript" ? "bg-green-500 text-white" : "" }`} onClick={JavascriptBtnClick}>Javascript</button>
+          <button className={`text-green-500 border-none outline-2 outline-green-500 py-1 px-5 rounded-lg font-bold text-lg cursor-pointer ${ btnsValue === "React" ? "bg-green-500 text-white" : "" }`} onClick={ReacttBtnClick}>React</button>
+          <button className={`text-green-500 border-none outline-2 outline-green-500 py-1 px-5 rounded-lg font-bold text-lg cursor-pointer ${ btnsValue === "C++" ? "bg-green-500 text-white" : "" }`} onClick={CppBtnClick}>C++</button>
+        </div>
       </div>
       <div className='flex gap-5 mt-5 flex-wrap justify-center items-center'>
         {projectsToShow && projectsToShow.map((project) => <ProjectCard key={project.title} projectDescription={project}/>)}
